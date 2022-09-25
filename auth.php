d8396463a637988c993f223944687e2b8584d180
algonchoveriba <nigraalfa141@gmail.com>
2022-09-26 00:23:16 +0900

    update08

--- auth.php
+++ auth.php
@@ -1,4 +1,4 @@
-!<?php
+<?php
 
 //==========================
 // ログイン認証・自動ログアウト
@@ -8,20 +8,20 @@ if( !empty($_SESSION['login_date']) ){
   debug('ログイン済みユーザーです。');
 
   // 現在日時が最終ログイン日時＋有効期限を超えていた場合
-  if(($_SESSION['login_date'] + $_SESSION['login_limit']) < time()){
-    debug('ログイン期限オーバーです。');
+  if( ($_SESSION['login_date'] + $_SESSION['login_limit']) < time()){
+    debug('ログイン有効期限オーバーです。');
 
     // セッションを削除（ログアウトする）
     session_destroy();
     // ログインページへ
     header("Location:login.php");
   }else{
-    debug('ログイン有効期限内です。');
+    debug('ログイン有効期限以内です。');
     // 最終ログイン日時を現在日時に更新
     $_SESSION['login_date'] = time();
 
     // 現在実行中のスクリプトファイル名がlogin.phpの場合
-    // $_SESSION['PHP_SELF']はドメインからのパスを返すため、今回だと「/practice/login.php」が返ってくるので、
+    // $_SERVER['PHP_SELF']はドメインからのパスを返すため、今回だと「/practice/login.php」が返ってくるので、
     // さらにbasename関数を使うことでファイル名だけを取り出せる
     if(basename($_SERVER['PHP_SELF']) === 'login.php'){
       debug('マイページへ遷移します。');
